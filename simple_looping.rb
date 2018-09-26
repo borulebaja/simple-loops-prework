bf7dd4bd7e47619e53c3e58d84115f7468a48c61
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-26 02:50:52 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,7 +24,7 @@ def times_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
-times_iterator(number_of_times)
+
 
 
 def while_iterator(number_of_times)
