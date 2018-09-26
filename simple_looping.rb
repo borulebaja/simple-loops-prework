de49c07389a9d457d4ef579a47dce5b8feff33c3
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-26 03:27:06 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
-    if number_of_times >= 7
+  
       break 
   phrase = "Welcome to Flatiron School's Web Development Course!"
  end
