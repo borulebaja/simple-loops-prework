b7240664e236400a4047a3ccfd400d2787d8db72
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-26 04:39:08 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -64,9 +64,8 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-	counter = 1..number_of_times
-	for counter in number_of_times
-	puts phrase
+for counter in number_of_times
+	puts "#{phrase}"
  end
 end
   
